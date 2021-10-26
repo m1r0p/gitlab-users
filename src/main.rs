e7d783cd4bd13136b8814787f1b0ae497b744efe
Igor M <eyegor.m@gmail.com>
2021-10-26 15:48:45 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Client;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // This is where we will setup our HTTP client requests.
+
+    // Still inside `async fn main`...
+    let client = Client::new();
+
+    // Parse an `http::Uri`...
+    let uri = "http://httpbin.org/ip".parse()?;
+
+    // Await the response...
+    let resp = client.get(uri).await?;
+
+    println!("Response: {}", resp.status());
+
+    Ok(())
 }
