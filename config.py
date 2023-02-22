f2a780f286d5079610c7afb8a76fdefd291b8fed
Igor M <eyegor.m@gmail.com>
2023-02-22 13:29:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ VALID_DOMAINS = ['example.com', 'example2.com']
 GITLAB_VERSION_URL="https://" + GITLAB_BASE_URL + "/api/v4/version"
 
 ##### old squash settings
-OLD_SQUASH_BASE_URL="http://192.168.11.237:8030/squash"
+OLD_SQUASH_BASE_URL="http://<ip_or_name>:8030/squash"
 OLD_SQUASH_ADMIN_URL = OLD_SQUASH_BASE_URL + '/administration'
-OLD_SQUASH_USER="igor.matveyev"
-OLD_SQUASH_PASS="q123456_"
+OLD_SQUASH_USER="<your_user>"
+OLD_SQUASH_PASS="<your_pass>"
 
