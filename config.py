15e7ad554726ac8871702d010378309fee389933
Igor M <eyegor.m@gmail.com>
2023-02-22 13:25:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 ###### settings
 ##### GitLab settings
-GITLAB_BASE_URL = 'git.devx.kiev.ua'
+GITLAB_BASE_URL = 'git.example.com'
 GITLAB_USERS_URL = "https://" + GITLAB_BASE_URL + "/api/v4/users?per_page=200;page="
 AMOUNT_OF_PAGES = 2
-GITLAB_TOKEN='oBBErz6zG4Ju5GJnjzCF'
-DEVX_DOMAINS = ['developex.net', 'developex.com', 'developex.biz', 'devx.kiev.ua', 'uradvlady.com', 'git.devx.kiev.ua', 
-        'developex.org', 'swiftpoint.com', 'simbirsoft.com', 'developex.local', 'reviewmycontract.com', 'developex.com.ua',
-        'logitech.com']
+GITLAB_TOKEN='<your_token>'
+VALID_DOMAINS = ['example.com', 'example2.com']
 GITLAB_VERSION_URL="https://" + GITLAB_BASE_URL + "/api/v4/version"
 
 ##### old squash settings
